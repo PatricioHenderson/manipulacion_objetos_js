cea42cba7da25d8e2fb7a1dffe058d958c49fd20
patricio <patriciohv@getfairplay.com>
2023-06-24 15:43:26 -0600

    feat: ejercicios resueltos

--- ejercicios_practica/1- storage/js/script.js
+++ ejercicios_practica/1- storage/js/script.js
@@ -20,6 +20,21 @@ y leer los datos ingresados en "usuario" y "email".
 con los datos almacenados en memoria, a fin de darle la bienvenida al usuario logeado.
 
 */
+const button = document.querySelector("#btnIngresar");
+const usuario = document.querySelector("#usuario")
+const email = document.querySelector("#email")
+
+button.onclick = () => {
+    localStorage.setItem("usuario", usuario.value);
+    localStorage.setItem("email",email.value)
+    window.location.reload();
+}
+
+const usuarioLogeado = document.querySelector("#usuarioLogeado")
+const emailLogeado = document.querySelector("#emailLogeado")
+// emailLogeado.textContent = localStorage.getItem("email")
+// usuarioLogeado.textContent = localStorage.getItem("usuario");
+
 
 /* 2 - Enunciado
 
@@ -32,6 +47,23 @@ y deberá revelar la sección "logout" (quitar la clase hidden).
 
 */
 
+const user = localStorage.getItem("usuario");
+const correo = localStorage.getItem("email");
+const login = document.querySelector("#login");
+const logout = document.querySelector("#logout");
+(user == null ? console.log("usuario no logueado") : welcomeUser(user,correo))
+
+function welcomeUser (user, correo ) {
+    
+    login.setAttribute('class', "hidden");
+    logout.setAttribute('class', "")
+    usuarioLogeado.textContent = user;
+    emailLogeado.textContent = correo;
+    
+}
+
+
+
 /* 3 - Enunciado
 
 - Debera capturar el evento "onclick" del boton "Salir". Cuando este
@@ -41,3 +73,10 @@ volverse a cargar debería aparecer nuevamente la sección de bienvenida
 ya que no debería haber más datos en memoria cargados.
 
 */
+
+const btnSalir = document.querySelector("#btnSalir")
+btnSalir.onclick = () => {
+    localStorage.removeItem("usuario");
+    localStorage.removeItem("email");
+    window.location.reload();
+}
--- ejercicios_practica/2- array methods/js/script.js
+++ ejercicios_practica/2- array methods/js/script.js
@@ -19,6 +19,11 @@ un array de personas mayores o igual a 18 años.
 - Imprimir en consola el array resultante.
 
 */
+let arrayJson = JSON.parse(personasJSON)
+arrayJson = arrayJson.filter(persona => {
+    return persona.edad > 18
+});
+console.log(arrayJson)
 
 /* 2 - Enunciado
 
@@ -30,6 +35,18 @@ mecanismos que prefiera para agregar por cada persona el nombre y edad
 en el HTML (es decir, generar el contenido de forma dinámica).
 
 */
+let body = document.body;
+let parrafo = document.createElement("p");
+let array = []
+
+arrayJson.forEach(person => {
+    array.push(person.nombre+ " " + person.edad);
+    console.log(array)
+});
+parrafo.textContent = array
+body.appendChild(parrafo);
+
+
 
 /* 3 - BONUS TRACK
 
@@ -40,3 +57,37 @@ que sea un valor que se toma del HTML (un input).
 se dispare al presionar un botón en el HTML que usted agregue.
 
 */
+
+let selectedAge = document.createElement("input");
+let button = document.createElement("button");
+button.textContent = "Filtrar";
+let lineBreak = document.createElement("br");
+let newArrayJson = JSON.parse(personasJSON)
+let newParrafo = document.createElement("p");
+
+body.appendChild(selectedAge);
+body.appendChild(lineBreak);
+body.appendChild(button);
+body.appendChild(lineBreak);
+body.appendChild(newParrafo);
+
+button.onclick = () => {
+    filter(selectedAge.value);
+    render(newArrayJson);
+}
+
+function filter (age) {
+    newArrayJson = newArrayJson.filter(persona => {
+        return persona.edad > age
+    });
+}
+
+let newRendered  = []
+function render (newArrayJson) {
+    console.log(newArrayJson)
+    newArrayJson.forEach(people => {
+        newRendered.push(people.nombre+ " " + people.edad);
+    });
+    newParrafo.textContent = newRendered
+    body.appendChild(newParrafo)
+}
